846ee457970e36d9732ac5dd7fd8f0345c25df85
Christian-SB <christian_eilert@web.de>
2023-10-05 20:28:08 +0200

    corrected file path

--- R/connect_to_db.R
+++ R/connect_to_db.R
@@ -1,6 +1,5 @@
 #' Establishing a connection to the aikia database
 #'
-#' @param mydb A mydb connection from DBI::dbConnect
 #' @param database The database name
 #' @param user Your MariaDB Username
 #' @param password Your password
@@ -11,20 +10,26 @@
 #' @examples \dontrun{
 #' connect_to_db(user = "testuser", password = "password")
 #' }
-connect_to_db <- function(mydb, database = "fin_data", user=NULL, password=NULL){
+connect_to_db <- function(database = "fin_data", user=NULL, password=NULL){
 
   ip <- unlist(pingr::nsl("aikia.org")$answer$data)
 
-  Checkmydb <- tryCatch(DBI::dbIsValid(mydb),
-                        error=function(e) e)
-  if(inherits(Checkmydb, "simpleError")){
 
-      mydb <- DBI::dbConnect(RMariaDB::MariaDB(),
-                             host = ip,
-                             user = user,
-                             password = password,
-                             group = database,
-                             default.file = "C:/Users/Admin/Documents/Git Projects/aikia/my.cnf")
+  if(Sys.info()[1]=="Windows"){
+    mydb <- DBI::dbConnect(RMariaDB::MariaDB(),
+                           host = ip,
+                           user = user,
+                           password = password,
+                           group = database,
+                           default.file = "C:/Users/Admin/Documents/Git Projects/aikia/my.cnf")
+  } else {
+
+    mydb <- DBI::dbConnect(RMariaDB::MariaDB(),
+                           host = ip,
+                           user = user,
+                           password = password,
+                           group = database,
+                           default.file = paste0(here::here(),"/my.cnf"))
   }
 }
 
